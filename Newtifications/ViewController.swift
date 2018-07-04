50028df682d2f7b36dae12d07651b02a2eff68d1
Yuri Petukhov <gxoct@yandex.ru>
2018-07-04 14:23:26 +0300

    Local notifications added.

--- Newtifications/ViewController.swift
+++ Newtifications/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let simpleCategory = UNNotificationCategory(identifier: "category-id", actions: [], intentIdentifiers: [], options: [])
+        UNUserNotificationCenter.current().setNotificationCategories([simpleCategory])
     }
 
-
+    @IBAction func sendNotificationPressed(_ sender: UIButton) {
+        UNUserNotificationCenter.current().getNotificationSettings {
+            (settings) in
+            
+            guard settings.authorizationStatus == .authorized else { return }
+            
+            let content = UNMutableNotificationContent()
+            content.title = "Simple Title"
+            content.title = "Simple Subtitle"
+            content.body = "Simple Body"
+            content.sound = .default
+            
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+            let uuid = UUID().uuidString
+            let request = UNNotificationRequest(identifier: uuid, content: content, trigger: trigger)
+            
+            UNUserNotificationCenter.current().add(request, withCompletionHandler: {
+                (error) in
+                
+                
+            })
+        }
+    }
+    
 }
 
